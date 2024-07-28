7cf70a5fd8ae0d648cdaf12e42d372a1cb3bc9f3
thegreystone <marcus@hirt.se>
2024-07-28 18:56:29 +0200

    Version tags

--- dockerize-native.sh
+++ dockerize-native.sh
@@ -2,5 +2,9 @@
 export DOCKER_BUILDKIT=1
 docker buildx create --name multi-arch-builder --use
 docker buildx inspect --bootstrap
+
+# Extract version from pom.xml
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 ./mvnw package -Pnative
-docker buildx build -f Dockerfile.native --platform linux/amd64,linux/arm64 -t greystone/slogan-generator:latest --push .
+docker buildx build -f Dockerfile.native --platform linux/amd64,linux/arm64 -t greystone/slogan-generator:$VERSION-native -t greystone/slogan-generator:latest-native --push .
--- dockerize.sh
+++ dockerize.sh
@@ -2,5 +2,9 @@
 export DOCKER_BUILDKIT=1
 docker buildx create --name multi-arch-builder --use
 docker buildx inspect --bootstrap
+
+# Extract version from pom.xml
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 ./mvnw package
-docker buildx build -f Dockerfile.jvm --platform linux/amd64,linux/arm64 -t greystone/slogan-generator:latest --push .
+docker buildx build -f Dockerfile.jvm --platform linux/amd64,linux/arm64 -t greystone/slogan-generator:$VERSION -t greystone/slogan-generator:latest --push .
